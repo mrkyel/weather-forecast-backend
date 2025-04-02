e2e9418f2e47fb71e172a2e12fd464e1fed03836
kale <kyel100@naver.com>
2025-04-02 14:50:09 +0900

    fix: 빌드오류 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,17 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { AirQualityModule } from './air-quality/air-quality.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      cache: true,
+    }),
+    CacheModule.register({
+      ttl: parseInt(process.env.CACHE_TTL) || 900,
       isGlobal: true,
     }),
     ScheduleModule.forRoot(),
